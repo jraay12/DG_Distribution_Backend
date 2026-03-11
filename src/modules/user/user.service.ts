e5dcdcac7ab804b93e71882afa3dd65f4bac5561
John Ray <johnraycaete@gmail.com>
2026-03-11 23:38:21 +0800

    feat(user): hash password when creating user using Bcrypt helper

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,13 +2,18 @@ import { CreateUserDTO } from "./dto/CreateUserDTO";
 import { UserReponseDTO } from "./dto/UserResponseDTO";
 import { UserRepository } from "./user.repository";
 import { User } from "./user.entity";
+import { Bcrypt } from "../../utils/bcrypt";
 export class UserService {
-  constructor(private userRepo: UserRepository) {}
+  constructor(
+    private userRepo: UserRepository,
+    private bcrypt: Bcrypt,
+  ) {}
 
   async createUser(data: CreateUserDTO): Promise<UserReponseDTO> {
     const user = User.create(data);
-
+    const hashPassword = await this.bcrypt.hash(user.password);
+    user.setPassword(hashPassword);
     await this.userRepo.createUser(user);
-    return user.toSafeObject()
+    return user.toSafeObject();
   }
 }
