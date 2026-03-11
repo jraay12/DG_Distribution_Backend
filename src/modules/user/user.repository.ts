3a5c89b465933c5ce216bcc66614b3079b03828a
John Ray <johnraycaete@gmail.com>
2026-03-11 22:52:37 +0800

    feat: add findById method in UserRepository

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -10,12 +10,24 @@ export class UserRepository {
         id: user.id,
         name: user.name,
         email: user.email,
-        password: user.password, 
+        password: user.password,
         role: user.role,
         isActive: user.isActive,
         createdAt: user.createdAt,
         updatedAt: user.updatedAt,
       },
     });
+  }
+
+  async findById(user_id: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: user_id,
+      },
+    });
+
+    if (!user) return null;
+
+    return User.reconstitute(user);
   }
 }
