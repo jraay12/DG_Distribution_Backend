fceea5c126561d58f189f69a499075640778302c
John Ray <johnraycaete@gmail.com>
2026-03-12 11:57:10 +0800

    feat(user-service): add conflict check when creating user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,6 +3,7 @@ import { UserReponseDTO } from "./dto/UserResponseDTO";
 import { UserRepository } from "./user.repository";
 import { User } from "./user.entity";
 import { Bcrypt } from "../../utils/bcrypt";
+import { ConflictError } from "../../utils/error/ConflictError";
 export class UserService {
   constructor(
     private userRepo: UserRepository,
@@ -10,6 +11,10 @@ export class UserService {
   ) {}
 
   async createUser(data: CreateUserDTO): Promise<UserReponseDTO> {
+    const emailExist = await this.userRepo.findByEmail(data.email);
+
+    if (emailExist) throw new ConflictError("User already exists");
+
     const user = User.create(data);
     const hashPassword = await this.bcrypt.hash(user.password);
     user.setPassword(hashPassword);
