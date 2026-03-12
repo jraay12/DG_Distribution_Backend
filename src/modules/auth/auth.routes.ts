5413faeac296149663cfd56407601a2cb5a633f9
John Ray <johnraycaete@gmail.com>
2026-03-12 14:14:36 +0800

    feat(auth): add refresh route in auth routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -5,8 +5,9 @@ const authRoutes = (authContoller: AuthController): Router => {
   const routes = Router();
 
   routes.post("/login", authContoller.login);
-  routes.post("/logout", authContoller.logout)
-  return routes
+  routes.post("/logout", authContoller.logout);
+  routes.post("/refresh", authContoller.refresh);
+  return routes;
 };
 
 export default authRoutes;
