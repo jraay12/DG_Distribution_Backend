6a825e1a43d6dfdf814347157228c59e19e121b1
John Ray <johnraycaete@gmail.com>
2026-03-12 12:28:40 +0800

    feat: add updatePassword method

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,6 +4,9 @@ import { UserRepository } from "./user.repository";
 import { User } from "./user.entity";
 import { Bcrypt } from "../../utils/bcrypt";
 import { ConflictError } from "../../utils/error/ConflictError";
+import { NotFoundError } from "../../utils/error/NotFoundError";
+import { ChangePasswordDTO } from "./dto/ChangePasswordDTO";
+import { BadRequestError } from "../../utils/error/BadRequestError";
 export class UserService {
   constructor(
     private userRepo: UserRepository,
@@ -20,5 +23,25 @@ export class UserService {
     user.setPassword(hashPassword);
     await this.userRepo.createUser(user);
     return user.toSafeObject();
+  }
+
+  async updatePassword(
+    user_id: string,
+    data: ChangePasswordDTO,
+  ): Promise<void> {
+    const user = await this.userRepo.findById(user_id);
+
+    if (!user) throw new NotFoundError("User not found!");
+
+    const passwordMatch = await this.bcrypt.compare(
+      data.oldPassword,
+      user.password,
+    );
+
+    if (!passwordMatch) throw new BadRequestError("Old password not match");
+
+    user.updatePassword(data.newPassword);
+    const hashedPassword = await this.bcrypt.hash(data.newPassword);
+    await this.userRepo.updatePassword(user_id, hashedPassword);
   }
 }
