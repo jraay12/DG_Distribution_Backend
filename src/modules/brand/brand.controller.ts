df9573e767d5fed1f3f88f7b390a1fca4c24fa58
John Ray <johnraycaete@gmail.com>
2026-03-23 14:42:36 +0800

    feat(controller/brand): add findById endpoint to return brand by id

--- src/modules/brand/brand.controller.ts
+++ src/modules/brand/brand.controller.ts
@@ -2,71 +2,90 @@ import { BrandService } from "./brand.service";
 import { Request, Response, NextFunction } from "express";
 
 export class BrandController {
-  constructor(private brandService: BrandService){}
+  constructor(private brandService: BrandService) {}
 
-  create = async(req: Request, res: Response, next: NextFunction) => {
+  create = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const input = req.body as {brand_name: string}
-      const user_id = req.user?.user_id
-      const result = await this.brandService.create(input, user_id)
+      const input = req.body as { brand_name: string };
+      const user_id = req.user?.user_id;
+      const result = await this.brandService.create(input, user_id);
       res.status(201).json({
         message: `Successfully create ${result.brand_name} brand`,
-        result
-      })
+        result,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 
-  delete = async(req: Request, res: Response, next: NextFunction) => {
+  delete = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const inputParams = req.params as {brand_id: string}
-      const user_id = req.user?.user_id
-      const result = await this.brandService.delete(inputParams, user_id)
+      const inputParams = req.params as { brand_id: string };
+      const user_id = req.user?.user_id;
+      const result = await this.brandService.delete(inputParams, user_id);
       res.status(200).json({
-        message: `Successfully deleted the ${result.brand_name} brand`
-      })
+        message: `Successfully deleted the ${result.brand_name} brand`,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 
-   restore = async(req: Request, res: Response, next: NextFunction) => {
+  restore = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const inputParams = req.params as {brand_id: string}
-      const user_id = req.user?.user_id
-      const result = await this.brandService.restore(inputParams.brand_id, user_id)
+      const inputParams = req.params as { brand_id: string };
+      const user_id = req.user?.user_id;
+      const result = await this.brandService.restore(
+        inputParams.brand_id,
+        user_id,
+      );
       res.status(200).json({
-        message: `Successfully restore ${result.brand_name} brand`
-      })
+        message: `Successfully restore ${result.brand_name} brand`,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-   }
+  };
 
-   getAllBrand = async(req: Request, res: Response, next: NextFunction) => {
+  getAllBrand = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const result = await this.brandService.getAllBrand()
+      const result = await this.brandService.getAllBrand();
       res.status(200).json({
-        result
-      })
+        result,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-   }
+  };
 
-   updateName = async(req: Request, res: Response, next: NextFunction) => {
+  updateName = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const inputBody = req.body as {brand_name: string}
-      const inputParams = req.params as {brand_id: string}
-      const user_id = req.user?.user_id
-      const result = await this.brandService.update(inputParams.brand_id, inputBody, user_id)
+      const inputBody = req.body as { brand_name: string };
+      const inputParams = req.params as { brand_id: string };
+      const user_id = req.user?.user_id;
+      const result = await this.brandService.update(
+        inputParams.brand_id,
+        inputBody,
+        user_id,
+      );
       res.status(200).json({
         message: "Successfully updated brand name",
-        result
-      })
+        result,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-   }
+  };
+
+  findById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const inputParams = req.params as { brand_id: string };
+      const result = await this.brandService.findById(inputParams.brand_id);
+      res.status(200).json({
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
