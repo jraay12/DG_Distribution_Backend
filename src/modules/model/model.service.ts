f52019039cf6ffcbc0e91a3f8a8db4142d07e3ed
John Ray <johnraycaete@gmail.com>
2026-03-24 10:46:40 +0800

    feat: add update method in the ModelService

--- src/modules/model/model.service.ts
+++ src/modules/model/model.service.ts
@@ -5,6 +5,7 @@ import { BrandRepository } from "../brand/brand.repository";
 import { UserRepository } from "../user/user.repository";
 import { CreateModelDTO } from "./dto/CreateModelDTO";
 import { ModelResponseDTO } from "./dto/ModelResponseDTO";
+import { UpdateModelDTO } from "./dto/UpdateModelDTO";
 import { Model } from "./model.entity";
 import { ModelRepository } from "./model.repository";
 
@@ -39,4 +40,28 @@ export class ModelService {
 
     return model.toJson();
   }
+
+  async update(data: UpdateModelDTO, user_id: string, model_id: string): Promise<ModelResponseDTO>{
+    const user = await this.userRepo.findById(user_id)
+
+    if(!user) throw new NotFoundError("User not found")
+
+    if (!user.isAdmin()) throw new ForbiddenError("You do not have permission to perform this action")
+
+    const brand = await this.brandRepo.findById(data.brand_id)
+
+    if (!brand) throw new NotFoundError("Brand not found")
+
+    if(brand.isDeleted) throw new BadRequestError("Brand is deleted")
+
+    const model = await this.modelRepo.findById(model_id)
+
+    if (!model) throw new NotFoundError("Model not found")
+
+    model.update(data.model_name, data.brand_id)
+
+    await this.modelRepo.update(model)
+
+    return model.toJson()
+  } 
 }
