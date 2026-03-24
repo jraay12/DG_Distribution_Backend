36416f4e3a0f7f4e1c6288d036c7f819c788b854
John Ray <johnraycaete@gmail.com>
2026-03-24 12:24:39 +0800

    feat: update model controller to support includeDeleted query param

--- src/modules/model/model.controller.ts
+++ src/modules/model/model.controller.ts
@@ -12,7 +12,7 @@ export class ModelController {
       const result = await this.modelService.create(input, user_id);
       res.status(200).json({
         message: "Successfully create new model",
-        data: result
+        data: result,
       });
     } catch (error) {
       next(error);
@@ -21,64 +21,75 @@ export class ModelController {
 
   update = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const inputBody : UpdateModelDTO = req.body as UpdateModelDTO
-      const inputParams = req.params as {model_id: string}
+      const inputBody: UpdateModelDTO = req.body as UpdateModelDTO;
+      const inputParams = req.params as { model_id: string };
       const user_id = req.user?.user_id;
 
-      const result = await this.modelService.update(inputBody, user_id, inputParams.model_id)
+      const result = await this.modelService.update(
+        inputBody,
+        user_id,
+        inputParams.model_id,
+      );
       res.status(200).json({
         message: "Successfully updated",
-        data: result
-      })
+        data: result,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 
-  getActiveModel = async (req: Request, res: Response, next: NextFunction) => {
+  getModels = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const result = await this.modelService.getActiveModel()
+      const includeDeleted = req.query.includeDeleted === 'true';
+      const result = await this.modelService.getModels(includeDeleted);
       res.status(200).json({
-        data: result
-      })
+        data: result,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 
   softDelete = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const inputParams = req.params as {model_id: string}
-      const result = await this.modelService.softDelete(inputParams.model_id)
+      const inputParams = req.params as { model_id: string };
+      const result = await this.modelService.softDelete(inputParams.model_id);
       res.status(200).json({
-        message: `Successfully delete ${result.model_name} model`
-      })
+        message: `Successfully delete ${result.model_name} model`,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 
   restore = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const inputParams = req.params as {model_id: string}
-      const result = await this.modelService.restore(inputParams.model_id)
+      const inputParams = req.params as { model_id: string };
+      const result = await this.modelService.restore(inputParams.model_id);
       res.status(200).json({
-        message: `Successfully restore ${result.model_name} model`
-      })
+        message: `Successfully restore ${result.model_name} model`,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 
-  findByIdWithBrand = async (req: Request, res: Response, next: NextFunction) => {
+  findByIdWithBrand = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
-      const inputParams = req.params as {model_id: string}
-      const result = await this.modelService.findByIdWithBrand(inputParams.model_id)
+      const inputParams = req.params as { model_id: string };
+      const result = await this.modelService.findByIdWithBrand(
+        inputParams.model_id,
+      );
       res.status(200).json({
-        data: result
-      })
+        data: result,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 }
