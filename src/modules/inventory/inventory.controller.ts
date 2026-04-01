416c6f14888180be5f683dc59b762722c2a2c0c5
John Ray <johnraycaete@gmail.com>
2026-04-02 00:48:49 +0800

    feat: add user_id in the deductStock method

--- src/modules/inventory/inventory.controller.ts
+++ src/modules/inventory/inventory.controller.ts
@@ -23,13 +23,14 @@ export class InventoryController {
   deductStock = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { product_id } = req.params as { product_id: string };
+      const user_id = req.user.user_id
       const { quantity } = req.body as {
         quantity: number;
       };
       const result = await this.inventoryService.deductStock({
         product_id,
         quantity,
-      });
+      }, user_id);
       res.status(200).json({ result });
     } catch (error) {
       next(error);
