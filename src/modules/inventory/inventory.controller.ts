83c38ab6ae55345969a316f38c6e607fd45e6f5a
John Ray <johnraycaete@gmail.com>
2026-04-01 09:15:17 +0800

    feat: create a controller calling the inventory service addStock method

---
+++ src/modules/inventory/inventory.controller.ts
@@ -0,0 +1,21 @@
+import { InventoryService } from "./inventory.services";
+import { Request, Response, NextFunction } from "express";
+export class InventoryController {
+  constructor(private inventoryService: InventoryService) {}
+
+  addStock = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { product_id } = req.params as { product_id: string };
+      const { quantity } = req.body as {
+        quantity: number;
+      };
+      const result = await this.inventoryService.addStock({
+        product_id,
+        quantity,
+      });
+      res.status(200).json({ result });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
