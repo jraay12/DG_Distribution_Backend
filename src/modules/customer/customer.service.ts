ff779b922b384160790015bd7e6a355fc0f83699
John Ray <johnraycaete@gmail.com>
2026-04-03 21:40:12 +0800

    feat(customer-service): add update method in the customer service

--- src/modules/customer/customer.service.ts
+++ src/modules/customer/customer.service.ts
@@ -1,7 +1,9 @@
+import { NotFoundError } from "../../utils/error/NotFoundError";
 import { Customer } from "./customer.entity";
 import { CustomerRepository } from "./customer.repository";
 import { CreateCustomerDTO } from "./dto/CreateCustomerDTO";
 import { CustomerResponseDTO } from "./dto/CustomerResponseDTO";
+import { UpdateCustomerDTO } from "./dto/UpdateCustomerDTO";
 
 export class CustomerService {
   constructor(private customerRepo: CustomerRepository){}
@@ -10,6 +12,18 @@ export class CustomerService {
     const customer = Customer.create(data)
 
     await this.customerRepo.save(customer)
+
+    return customer.toJSON()
+  }
+
+  async update(data: UpdateCustomerDTO, customer_id: string): Promise<CustomerResponseDTO> {
+    const customer = await this.customerRepo.findById(customer_id)
+
+    if (!customer) throw new NotFoundError("Customer not found")
+
+    customer.updateCustomer(data)
+
+    await this.customerRepo.update(customer)
 
     return customer.toJSON()
   }
