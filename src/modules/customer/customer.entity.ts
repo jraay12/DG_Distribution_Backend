32424b5c2b61d7e5debab9f5ebf67c686ed89fa1
John Ray <johnraycaete@gmail.com>
2026-04-03 21:39:10 +0800

    feat(customer-entity): add updateCustomer method in the customer entity

--- src/modules/customer/customer.entity.ts
+++ src/modules/customer/customer.entity.ts
@@ -35,6 +35,21 @@ export class Customer {
 
     return new Customer({ ...props, id: crypto.randomUUID() });
   }
+
+  updateCustomer(props: Omit<CustomerProps, "id" | "createdAt" | "updatedAt" | "deletedAt">){
+    if (!props.store_name) throw new BadRequestError("Store name is required");
+    if (!props.owner_name) throw new BadRequestError("Owner name is required");
+    if (!props.contact_number)
+      throw new BadRequestError("Contact number is required");
+    if (!props.address)
+      throw new BadRequestError("Address is required");
+
+    this.props.store_name = props.store_name
+    this.props.owner_name = props.owner_name
+    this.props.address = props.address
+    this.props.contact_number = props.contact_number
+    this.props.updatedAt = new Date()
+  }
   
   static hydrate(props: CustomerProps):Customer {
     return new Customer(props)
