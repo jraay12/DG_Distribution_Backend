f8a80e4c35ddcaa07367bcf582ff780d64d362ad
John Ray <johnraycaete@gmail.com>
2026-04-05 22:21:12 +0800

    feat(image evidence entity): add image evidence entity

---
+++ src/modules/delivery/image-evidence.entity.ts
@@ -0,0 +1,49 @@
+import { BadRequestError } from "../../utils/error/BadRequestError"
+
+export interface ImageEvidenceProps {
+  id: string
+  delivery_id: string
+  image_path: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export class ImageEvidence {
+  private props: ImageEvidenceProps
+
+  constructor(props: ImageEvidenceProps) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? new Date(),
+    }
+  }
+
+  static save(props: Omit<ImageEvidenceProps, "id" | "createdAt" | "updatedAt">) {
+    if(!props.delivery_id) throw new BadRequestError("Delivery ID is required")
+    if(!props.image_path) throw new BadRequestError("Image path is required")
+
+    return new ImageEvidence({...props, id: crypto.randomUUID()})
+  }
+
+  // Getters
+  get id(): string {
+    return this.props.id
+  }
+
+  get deliveryId(): string {
+    return this.props.delivery_id
+  }
+
+  get imagePath(): string {
+    return this.props.image_path
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt!
+  }
+
+  get updatedAt(): Date {
+    return this.props.updatedAt!
+  }
+}
