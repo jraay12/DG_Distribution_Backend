af35df44ca0f3b504f67f6281fd4fde3e913734c
John Ray <johnraycaete@gmail.com>
2026-05-12 12:41:45 +0800

    feat: add repository for product recommendation

--- src/modules/Stock Movement/stock-movement.repository.ts
+++ src/modules/Stock Movement/stock-movement.repository.ts
@@ -18,9 +18,7 @@ export class StockMovementRepository {
         created_by: stockMovement.createdBy,
         createdAt: stockMovement.createdAt,
         product_id: stockMovement.productId,
-        store_id: stockMovement.storeId ?? null
-        
-        
+        store_id: stockMovement.storeId ?? null,
       },
     });
   }
@@ -39,8 +37,35 @@ export class StockMovementRepository {
         created_by: movement.createdBy,
         createdAt: movement.createdAt,
         product_id: movement.productId,
-        store_id: movement.storeId ?? null
+        store_id: movement.storeId ?? null,
       })),
+    });
+  }
+
+  async getTopOutMovements(customer_id: string) {
+    const now = new Date();
+
+    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+    const startOfNextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+    return this.prisma.stockMovement.groupBy({
+      by: ["product_id"],
+      where: {
+        store_id: customer_id,
+        type: "OUT",
+        createdAt: {
+          gte: startOfMonth,
+          lt: startOfNextMonth,
+        },
+      },
+      _sum: {
+        quantity: true,
+      },
+      orderBy: {
+        _sum: {
+          quantity: "desc",
+        },
+      },
+      take: 3,
     });
   }
 }
